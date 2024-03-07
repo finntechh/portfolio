1765a528604666a1baf9356b8c1a72787a07d6b5
Finn <fhertsch235@gmail.com>
2024-03-07 17:42:52 +0100

    complete setup

--- main/build/views.py
+++ main/build/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "title": "Home"
+    }
+    return render(request, 'home.html', context)
+
