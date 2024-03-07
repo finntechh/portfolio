18b0160962807c3222a832103500140cc9bf97b2
Finn <fhertsch235@gmail.com>
2024-03-07 18:05:47 +0100

    complete setup

--- main/build/urls.py
+++ main/build/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="Index"),
+    path('', views.home, name="home"),
+    path('Mitglied-werden/', views.mitglied, name="mitglied"),
+    path('Über-uns/', views.uns, name="uns"),
+    path('Kontakt/', views.kontakt, name="kontakt"),
 ]
--- main/build/views.py
+++ main/build/views.py
@@ -3,9 +3,28 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 
-def index(request):
+def home(request):
     context = {
         "title": "Home"
     }
     return render(request, 'home.html', context)
+
+def uns(request):
+    context = {
+        "title": "Über Uns"
+    }
+    return render(request, 'uns.html', context)
+
+def kontakt(request):
+    context = {
+        "title": "Kontakt"
+    }
+    return render(request, 'kontakt.html', context)
+
+def mitglied(request):
+    context = {
+        "title": "Mitglied werden"
+    }
+    return render(request, 'mitglied.html', context)
+
 
